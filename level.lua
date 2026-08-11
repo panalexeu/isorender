459adb1e7e14d263ebbebb4c1e03eca0316dbeff
Oleksii Horchynskyi <pan.alexeu@gmail.com>
2026-08-12 00:12:43 +0300

    save my best attempt to implement layered isometric rendering

--- level.lua
+++ level.lua
@@ -2,18 +2,32 @@
 
 function level_load() 
     game_state = "level"
+    origin_x = 32
+    origin_y = 0
     tile_size_x = 16
-    tile_size_y = 8 
+    tile_size_y = 16
+    diamond_w = 8 
+    diamond_h = 4
+    diamond_elavation = 8
     level_objects = {
         tiles = {},
     } 
-    local tilemap = {
-        {4, 1, 1, 1}, 
-        {0, 1, 1, 0}, 
-        {0, 1, 2, 0}, 
-        {0, 3, 1, 0}
+    local layers = {
+        {
+            {2, 2, 2, 2}, 
+            {2, 2, 2, 2}, 
+            {2, 2, 2, 2}, 
+            {2, 2, 2, 2}
+        },
+        {
+            {0, 0, 0, 0, 3, 3, 3, 3}, 
+            {0, 0, 0, 0, 3, 0, 0, 3}, 
+            {0, 0, 0, 0, 3, 0, 0, 3}, 
+            {0, 0, 0, 0, 3, 0, 0, 3},
+            {3, 3, 3, 3, 3, 0, 0, 3}
+        }
     }
-    load_map(tilemap)
+    load_map(layers)
 end 
 
 function level_draw()
@@ -27,17 +41,21 @@ end
 function level_update(dt) 
 end
 
-function load_map(tilemap)
-    for i=1,#tilemap  do 
-        for j=1,#tilemap[i] do 
-            local value = tilemap[i][j]
-            if value > 0 then
-                local x_pos = j * tile_size_x
-                local y_pos = i * tile_size_y
-                local quad = quads.tiles[value]
-                local tile_ = tile:new(x_pos, y_pos, tile_size_x, tile_size_y, quad)
-                table.insert(level_objects.tiles, tile_)
+function load_map(layers)
+    for k=1,#layers do
+        local layer = layers[k]
+        for i=1,#layer do 
+            for j=1,#layer[i] do 
+                local value = layer[i][j]
+                if value > 0 then
+                    local x_proj = ((j - i) * diamond_w) + origin_x
+                    local y_proj = ((i + j) * diamond_h) + (k * diamond_elavation) + origin_y
+                    print(k * diamond_elavation)
+                    local quad = quads.tiles[value]
+                    local tile_ = tile:new(x_proj, y_proj, diamond_w, diamond_h, quad)
+                    table.insert(level_objects.tiles, tile_)
+                end 
             end 
         end 
-    end 
+    end
 end 
